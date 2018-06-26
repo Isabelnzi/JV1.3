41083a537063a1a87126727a6cec4dbfb42b503c
Nzingani <isabelnzingani@outlook.com>
2018-06-26 13:07:38 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//declaration de fonction et de changement de couleur du texte en vert
+function changeColor(){
+document.getElementById('text').style.color = 'green';
+}
+//changement du couleur de texte en rouge
+function primaryColor(){
+document.getElementById('text').style.color = 'red';
+}
+//changement du couleur de texte en bleu
+function secondarycColor(){
+document.getElementById('text').style.color = 'blue';
+}
